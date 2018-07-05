6106a39cfdbe76a030b9fcdeb0997ce90268b102
Douglas Renato <douglasrenatopro@gmail.com>
2018-07-05 04:42:47 +0000

    ajuste noo ocultador de console.log em produção

--- app/components/HideConsoleLog.js
+++ app/components/HideConsoleLog.js
@@ -4,37 +4,38 @@ function HideConsoleLog () {
   return (
     <div>
       {
-        // se não for local
-        (window.location.hostname !== 'localhost' || window.location.origin !== 'file://') && 
-        // e não tiver paremetro ?dev na url
-        (window.location.search !== '?dev') ?
-          // oculta todos e quaisquer console.log's
-          console.assert = 
-            console.clear = 
-              console.constructor = 
-                console.count = 
-                  console.debug =  
-                    console.dir = 
-                      console.dirxml = 
-                        console.error = 
-                          console.group = 
-                            console.groupCollapsed = 
-                              console.groupEnd = 
-                                console.info = 
-                                  console.log = 
-                                    console.markTimeline = 
-                                      console.profile = 
-                                        console.profileEnd = 
-                                          console.table = 
-                                            console.time = 
-                                              console.timeEnd = 
-                                                console.timeStamp = 
-                                                  console.timeline = 
-                                                    console.timelineEnd = 
-                                                      console.trace = 
-                                                        console.warn = 
-                                                          function() { return false } : 
-          null
+        // se for local mostra os console.log's
+        window.location.hostname === 'localhost' || window.location.origin === 'file://' ? 
+          null : 
+            // e não tiver paremetro ?dev na url
+            window.location.search !== '?dev' ?
+              // oculta todos e quaisquer console.log's
+              console.assert = 
+                console.clear = 
+                  console.constructor = 
+                    console.count = 
+                      console.debug =  
+                        console.dir = 
+                          console.dirxml = 
+                            console.error = 
+                              console.group = 
+                                console.groupCollapsed = 
+                                  console.groupEnd = 
+                                    console.info = 
+                                      console.log = 
+                                        console.markTimeline = 
+                                          console.profile = 
+                                            console.profileEnd = 
+                                              console.table = 
+                                                console.time = 
+                                                  console.timeEnd = 
+                                                    console.timeStamp = 
+                                                      console.timeline = 
+                                                        console.timelineEnd = 
+                                                          console.trace = 
+                                                            console.warn = 
+                                                              function() { return false } : 
+              null
       }
     </div>
   )
